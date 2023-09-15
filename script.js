a60735c3a17a51f20b03f144f8a6365d6dd69e02
Margulis162 <markmargulis@proton.me>
2023-09-14 21:09:55 -0400

    randomizer function

---
+++ script.js
@@ -0,0 +1,11 @@
+// var 
+const txt = document.getElementById("shadow");
+
+// f(x)
+function random(min, max){
+    num = Math.floor(Math.random()*(max - min + 1) + min);
+    return num;
+}
+// flow
+random( 1, 4);
+console.log(num);
