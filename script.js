939239828b5c2fd58eb0edb2e8f694c6254ff6f1
Margulis162 <markmargulis@proton.me>
2023-09-14 21:43:36 -0400

    some minor style ajustments

--- script.js
+++ script.js
@@ -8,14 +8,15 @@ function random(min, max){
 }
 
 function shadow(){
-    let shadows ="";
+    let shadows ="2px 2px  black, -2px -2px black,5px 5px 10px black, -5px -5px 10px black,";
     let offset = 2;
     for(let i = 0; i <800; i++){
-        range_num_1 = random(0, 255);
-        range_num_2 = random(0, 255);
-        range_num_3 = random(0, 255);
+        // max less than 255 to make colors less bright, inbamance 
+        let red = random(0, 150);
+        let green = random(0, 150);
+        let  blue = random(0, 250);
        
-        shadows += `1px ${offset}px rgb(${range_num_1}, ${range_num_2}, ${range_num_3}),`;
+        shadows += `1px ${offset}px rgb(${red}, ${green}, ${blue}),`;
         offset += 1;
         
     }
